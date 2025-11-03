4a5e8459827969ed5419e7807707dec91c7c072e
Mikael <ms2379177@gmail.com>
2025-11-03 20:11:38 -0300

    Add files via upload

---
+++ Cassino.py
@@ -0,0 +1,20 @@
+## Feito Com Ajuda De Terceiros!!
+jogador = input("Seu Nome?: ")
+poupanca = int(input("Quanto dinheiro você quer depositar? (máximo R$1000)"))
+if poupanca > 1000 or poupanca < 0:
+    print("Valor indefinido. Definindo para R$500")
+    poupanca = 500
+import random
+num_maquina = random.randint(1,5)
+while True:
+    palpite = int(input("Para jogar é simples apenas tente adivinhar o número que a máquina escolheu entre 1 a 5: "))
+    if palpite == num_maquina:
+        print("Parabéns você ganhou 200 reais!")
+        poupanca += 200
+        num_maquina = random.randint(1, 5)
+    elif palpite < num_maquina or palpite > num_maquina:
+        print(f"Você errou! Tente novamente")
+        poupanca -= 100
+    if poupanca <= 0:
+        print("Seu dinheiro acabou! Jogo encerrado.")
+        break
